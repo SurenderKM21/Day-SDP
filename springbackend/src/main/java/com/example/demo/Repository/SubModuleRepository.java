7dc04b0321414714bbe762d12a6e4b9cfbae5295
Yashashwini11 <125336223+Yashashwini11@users.noreply.github.com>
2024-08-05 12:30:03 +0530

    Delete springbackend directory

--- springbackend/src/main/java/com/example/demo/Repository/SubModuleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.SubModule;
-
-public interface SubModuleRepository extends JpaRepository<SubModule, Integer> {
-
-}
