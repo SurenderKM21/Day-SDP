4d47e52a6ff962440221c4e55cf27e3813e7b0dc
Yashashwini11 <125336223+Yashashwini11@users.noreply.github.com>
2024-08-05 19:32:29 +0530

    Delete sdp-backend directory

--- sdp-backend/src/main/java/com/example/demo/Repository/AssignmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.Assignment;
-
-public interface AssignmentRepository extends JpaRepository<Assignment, Integer> {
-
-}
